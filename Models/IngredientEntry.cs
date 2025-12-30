140ca4667d3bcf817559d6b1499051d0b94ab1e2
zacks <zack.stehura@gmail.com>
2025-12-30 17:39:30 -0500

    fix commit tracking clean up UI

--- Models/IngredientEntry.cs
+++
@@ -1,9 +0,0 @@
-namespace RecipesApp.Models;
-
-public class IngredientEntry
-{
-    public string Name { get; set; } = string.Empty;
-    public decimal? Quantity { get; set; }
-    public MeasurementUnit Unit { get; set; } = MeasurementUnit.Pieces;
-}
-
